c58b1eaf50b54a17a6e2c2a399a6bbf4dc3c2070
Suho Kim <skim2@fnal.gov>
2019-12-16 15:02:12 -0600

    Tau samples

--- submitters/submitjobs.sh
+++ submitters/submitjobs.sh
@@ -225,6 +225,38 @@ samples=(  \
 ##  "ZH_HToSSTodddd_ZToLL_MH-125_MS-55_ctauS-10"       \
 ##  "ZH_HToSSTodddd_ZToLL_MH-125_MS-55_ctauS-100"      \
 ##  "ZH_HToSSTodddd_ZToLL_MH-125_MS-55_ctauS-1000"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-1"      \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-10"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-100"    \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-1000"   \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-1"        \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-10"       \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-100"      \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-7_ctauS-1000"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-1"      \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-10"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-100"    \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-1000"   \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-1"        \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-10"       \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-100"      \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-15_ctauS-1000"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-1"      \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-10"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-100"    \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-1000"   \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-1"        \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-10"       \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-100"      \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-40_ctauS-1000"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-1"      \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-10"     \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-100"    \
+##  "ggZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-1000"   \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-1"        \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-10"       \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-100"      \
+##  "ZH_HToSSTo4Tau_ZToLL_MH-125_MS-55_ctauS-1000"     \
 
 )
 
